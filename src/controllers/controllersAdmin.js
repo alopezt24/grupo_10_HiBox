43821e645603aaae30919e5fbb78f713be55ef95
NicoNovikov <niconovikov@gmail.com>
2022-03-02 00:26:16 -0300

    Edit & Upload Product

--- src/controllers/controllersAdmin.js
+++ src/controllers/controllersAdmin.js
@@ -23,6 +23,9 @@ const controllers = {
         res.render(path.resolve(__dirname, '../views/admin/productDetail'), {miProducto})
     },
     save: (req,res) =>{
+        let ultimoProducto = products.pop();
+        products.push (ultimoProducto);
+
         let img
 			if(req.file != undefined){
 				img = "/images/products/" + req.file.filename
@@ -30,18 +33,18 @@ const controllers = {
 				img = '/images/products/default-image.jpg'
 			}
 
-        let ultimoProducto = products.pop();
-        products.push (ultimoProducto);
         let nuevoProducto = {
             id: ultimoProducto.id +1,
-            nombre: req.body.nombre,
+            /*nombre: req.body.nombre,
             categoria: req.body.categoria,
             subcategoria: req.body.subcategoria,
             precio: req.body.precio,
             precioAnterior: req.body.precioAnterior,
             detalle: req.body.detalle,
             img: "/images/products/" + req.file.filename,
-            estado: req.body.estado   
+            estado: req.body.estado   */
+            ...req.body,
+            img
         }
         products.push(nuevoProducto);
         let nuevoProductoGuardar = JSON.stringify( products, null , 2);
@@ -54,7 +57,32 @@ const controllers = {
         res.render (path.resolve(__dirname,'../views/admin/productEdit'), {productoEditar});
     },
     update: (req,res) =>{
-        req.body.id = req.params.id;
+        let id = req.params.id;
+        let productToEdit = products.find (product => product.id == id)
+
+        let img
+        if (req.file != undefined){
+            img = "/images/products/" + req.file.filename
+        } else {
+            img = "/images/products/default-image.jpg"
+        }
+
+        productToEdit = {
+            id: productToEdit.id,
+            ...req.body,
+            img
+        };
+
+        let newProducts = products.map (product => {
+if (product.id == productToEdit.id){
+return product = {...productToEdit};
+}
+return product;
+        })
+
+        fs.writeFileSync (productsFilePath, JSON.stringify (newProducts, null, ' '));
+        res.redirect('/admin/adminProduct');
+        /* req.body.id = req.params.id;
         req.body.img = req.file ? "/images/products/" + req.file.filename : req.body.oldImagen;
         let productosUpdate = products.map(producto =>{
             if(producto.id == req.body.id){
@@ -64,7 +92,7 @@ const controllers = {
         })
         let productoActualizar = JSON.stringify( productosUpdate , null , 2);
         fs.writeFileSync(path.resolve(__dirname,"../data/products.json" ), productoActualizar)
-        res.redirect('/admin/adminProduct');
+        res.redirect('/admin/adminProduct');*/
     },
     erase: (req,res) => {
         let cont = 1;
