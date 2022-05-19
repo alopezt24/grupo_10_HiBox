11bd577cf319d79821e22ff1c7015b91469ba93c
horaciogs <hgs.linux@gmail.com>
2022-05-19 20:37:29 -0300

    detalles react 2

--- dashboard/src/components/ProductList.js
+++ dashboard/src/components/ProductList.js
@@ -1,11 +1,15 @@
 import React from 'react';
-
+import {Link} from 'react-router-dom';
 function ProductList(props){
     return (
         <React.Fragment>
             <tr>
                 <td>{props.id}</td>
-                <td>{props.name}</td>
+                <td>
+                <Link to={"/ProductDetail/" + props.id }>
+                    <span>{props.name}</span>
+                </Link>
+                </td>
                 <td>{props.categorys.category}</td>
                 <td>{props.detail}</td>
                 <td>{props.price}</td>
