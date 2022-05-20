ebe85ad721b59eca64d9de7101e212e10d1013b7
NicoNovikov <niconovikov@gmail.com>
2022-05-20 17:41:44 -0300

    Sweet Alert Delete Products & Users

--- public/js/productDelete.js
+++ public/js/productDelete.js
@@ -1,19 +1,19 @@
 
 window.addEventListener('load', function(){
     console.log('conected!')
-    let deleteUser = document.querySelectorAll('.form-adminProduct');
+    let deleteProduct = document.querySelectorAll('.form-adminProduct');
    
-    deleteUser.forEach(form => {
+    deleteProduct.forEach(form => {
         form.addEventListener('click', e => {
             e.preventDefault();
             Swal.fire({
-                title: 'Estás por borrar un Usuario',
+                title: 'Estás por borrar un Producto',
                 text: "¡Recuerda que si lo borras no lo podrás recuperar!",
                 icon: 'warning',
                 showCancelButton: true,
                 confirmButtonColor: '#7d92a5',
                 cancelButtonColor: '#fa5f63',
-                confirmButtonText: 'Borrar Usuario'
+                confirmButtonText: 'Borrar Producto'
               }).then((result) => {
                 if (result.isConfirmed) {
                     form.submit();
