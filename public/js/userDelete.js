d38f1064d45ee1fd7233c960fcac5c5cc080f8c7
NicoNovikov <niconovikov@gmail.com>
2022-05-20 17:27:24 -0300

    User Delete

---
+++ public/js/userDelete.js
@@ -0,0 +1,25 @@
+
+window.addEventListener('load', function(){
+    console.log('conected!')
+    let deleteForm = document.querySelectorAll('.form-adminProduct');
+   
+    deleteForm.forEach(form => {
+        form.addEventListener('click', e => {
+            e.preventDefault();
+            Swal.fire({
+                title: 'Estas seguro?',
+                text: "No vas a poder recuperar el usuario!",
+                icon: 'warning',
+                showCancelButton: true,
+                confirmButtonColor: '#3085d6',
+                cancelButtonColor: '#d33',
+                confirmButtonText: 'Si, Borrar Usuario!'
+              }).then((result) => {
+                if (result.isConfirmed) {
+                    form.submit();
+                }
+              })
+            
+        })
+    })
+});
